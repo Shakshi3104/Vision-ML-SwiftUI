c1107988eecca431e3ad606173d24372b56357e3
Shakshi3104 <pikanchisato@yahoo.co.jp>
2021-07-11 11:57:39 +0900

    Initial Commit

---
+++ Vision+ML SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Vision+ML SwiftUI
+//
+//  Created by MacBook Pro on 2021/07/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Vision+ML SwiftUI/Vision_ML_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vision_ML_SwiftUIApp.swift
+//  Vision+ML SwiftUI
+//
+//  Created by MacBook Pro on 2021/07/11.
+//
+
+import SwiftUI
+
+@main
+struct Vision_ML_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
